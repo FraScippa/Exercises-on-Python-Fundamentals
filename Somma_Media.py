787bd2c05954d38a5ab83c30d6730b5f1cef1994
FraScippa <francy.scippa@gmail.com>
2024-02-22 18:53:04 +0100

    sommaRandom

--- Somma_Media.py
+++ Somma_Media.py
@@ -1,9 +1,10 @@
 
 #calcola la somma 
+import random
 
 lista=[]
 
-for x in range(1,1000001):
+"""for x in range(1,1000001):
     lista.append(x)
 
 SommaLista=sum(lista)
@@ -12,6 +13,26 @@ print(SommaLista)
 
 #calcola la media
 
+media=SommaLista/1000000
+
+print(media)"""
+
+
+#generatore randomico
+
+for y in range(1000001):
+    NumeroRandom=random.randint(1,1000001)
+    lista.append(NumeroRandom)
+
+SommaRandom=sum(lista)
+
+print(SommaRandom)
+
+#media 
+
+media=SommaRandom/1000000
+
+print(media)
 
 
 

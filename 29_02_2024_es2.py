b7c5f7177a0d80b010ab737fbd32d634fe1549bd
FraScippa <francy.scippa@gmail.com>
2024-02-29 19:00:20 +0100

    cose

--- 29_02_2024_es2.py
+++ 29_02_2024_es2.py
@@ -1,3 +1,60 @@
 #sapendo che la funzione random.randint (start,end)
 #genera un numero intero compreso tra start e end, end compreso,
-#
+#costruire una: lista di numeri casuali lunga 100 e 
+#stampare la somma di tutti i numeri
+
+
+"""import random
+
+lista=[]
+
+for y in range(100):
+    NumeroX=random.randint(1,10)  #randint: generatore numeri casuali da 1,10 (10 COMPRESO)
+    lista.append(NumeroX)
+
+SommaRandom=sum(lista)
+
+print(SommaRandom)
+
+ #somma tutti i valori secondo tipo
+
+totale = 1
+
+inc=[]
+
+for i in inc:
+    totale = totale * i
+
+print("Totale", totale)"""
+
+
+#Costruire due liste, la prima che contiene i numeri pari fino a 1000
+#la seconda che contiene i numeri dispari fino a 1000
+#a partire dalle prime due liste,
+#costruire una terza lista che contiene prima tutti i pari e poi tutti i dispari
+
+listaPari=[]
+
+for x in range(0,1001,2):
+    if(x%2 ==0):
+        print(x)
+
+listaDispari=[]       
+
+for y in range(1,1001,2):
+    if(y%2==1):
+        print(y)
+
+
+
+
+
+
+    
+
+
+
+
+
+
+

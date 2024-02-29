3f4ebefe744a6fe595f9397e54ea42f110fa88cc
FraScippa <francy.scippa@gmail.com>
2024-02-29 16:23:39 +0100

    bho2

--- 29_02_2024.py
+++ 29_02_2024.py
@@ -1,5 +1,10 @@
-print(10 // 3) #con il simbolo // stampa il risultato intero.
+"""print(10 // 3)""" #con il simbolo // stampa il risultato intero.
 
 #.remove : per togliere un elemento dalla lista
 #.pop : per rimuovere un elemento tramite la sua posizione. Per svuotare la lista --> .clear
+
+L1=10
+L2=L1
+L1=20
+print(L2)
 

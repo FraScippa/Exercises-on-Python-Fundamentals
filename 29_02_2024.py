b78069a54166724b6615f9cc395129282f46d475
FraScippa <francy.scippa@gmail.com>
2024-02-29 18:04:30 +0100

    cose

--- 29_02_2024.py
+++ 29_02_2024.py
@@ -3,8 +3,18 @@
 #.remove : per togliere un elemento dalla lista
 #.pop : per rimuovere un elemento tramite la sua posizione. Per svuotare la lista --> .clear
 
-L1=10
+"""L1=10
 L2=L1
 L1=20
-print(L2)
+print(L2)"""
 
+#copy. : per clonare una lista (duplica gli elementi)
+
+#tabellina del 23
+
+y = 23
+
+for x in range(1, 11):
+    print(y*x)    #end=" " : per scriverli tutti nella stessa riga
+
+print(list(map(lambda i: i * 23, v)))  #map? lambda?

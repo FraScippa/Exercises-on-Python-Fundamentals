0fefb08b942bd0c22bf89c27c7058a6c8b9be1b7
Francesca <159931178+FraScippa@users.noreply.github.com>
2024-04-17 19:49:35 +0200

    eser

--- Classe/Nuovo/esLezione3.py
+++ Classe/Nuovo/esLezione3.py
@@ -17,7 +17,7 @@ print(somma)'''
 
 ##########################################################################
 
-d = {'pippo':2, 'pluto':1, 'topolino':5}
+'''d = {'pippo':2, 'pluto':1, 'topolino':5}
 d1 = {}
 
 #d1 = pippo 0.25 pluto 1/8 topolino 5/8 [stesse chiavi ma i valori= valore vecchio / tutti i valori] output
@@ -25,9 +25,28 @@ d1 = {}
 somma = sum(list(d.values()))
 
 for key in d:
-    d1[key] = d[key]/somma
+    d1[key] = d[key]/somma #d1[key] tira fuori il valore
 
-print(d1)
+print(d1)'''
+
+#crea diz con 2 chiavi: somma pari e somma dispari. Questo sarà la somma dei n pari della prima e la somma dei n dispari
+
+l = [1,2,3,4,5,2,2,6,7,16]
+
+d2 = {'somma_pari':0, 'somma_dispari':0}
+
+for i in l:
+    if i % 2 == 0 :
+        d2['somma_pari'] += i 
+    else:
+        d2['somma_dispari'] += i
+
+print(d2) 
+
+
+
+
+
 
 
 
